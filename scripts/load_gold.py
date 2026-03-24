e683944142b3a1446ea1812c7e55755c51989330
flg29-data <ferna29@gmail.com>
2026-03-24 19:31:53 -0300

    Update load_gold.py

--- scripts/load_gold.py
+++ scripts/load_gold.py
@@ -1,1 +1,11 @@
 
+import pandas as pd
+from sqlalchemy import create_engine
+
+engine = create_engine("postgresql://postgres:postgres@localhost:5432/labdb")
+
+df = pd.read_parquet("data/silver/silver/retail_silver.parquet")
+
+df.to_sql("fact_sales", engine, if_exists="replace", index=False)
+
+print("Dados carregados no Postgres")
