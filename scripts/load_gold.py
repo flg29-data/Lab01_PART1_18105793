bd35190faa1c9d7f14b5d1a97cfcebe7650b7841
flg29-data <ferna29@gmail.com>
2026-03-24 19:39:54 -0300

    Update load_gold.py

--- scripts/load_gold.py
+++ scripts/load_gold.py
@@ -1,10 +1,9 @@
-
 import pandas as pd
 from sqlalchemy import create_engine
 
-engine = create_engine("postgresql://postgres:postgres@localhost:5432/labdb")
+engine = create_engine("postgresql://postgres:postgres@localhost:5432/lab_data")
 
-df = pd.read_parquet("data/silver/silver/retail_silver.parquet")
+df = pd.read_parquet("C:/Users/ferna/OneDrive/Laboratorio01/silver/retail_silver.parquet")
 
 df.to_sql("fact_sales", engine, if_exists="replace", index=False)
 
