5ed6603b8fcb2bf8eef41bf97ac5a048804cf0be
flg29-data <ferna29@gmail.com>
2026-03-24 02:55:09 -0300

    Update transform_silver.py

--- scripts/transform_silver.py
+++ scripts/transform_silver.py
@@ -1,1 +1,33 @@
+import pandas as pd
 
+df = pd.read_csv("data/raw/raw/retail_raw.csv")
+
+# Padronizar nomes
+df.columns = (
+    df.columns
+    .str.strip()
+    .str.lower()
+    .str.replace(' ', '_')
+)
+
+# Converter data
+df['date'] = pd.to_datetime(df['date'])
+
+# Criar métricas
+df['total_value'] = df['total_items'] * df['total_cost']
+
+# Tratar nulos
+df['customer_name'] = df['customer_name'].fillna("UNKNOWN")
+df['promotion'] = df['promotion'].fillna("UNKNOWN")
+
+# Remover valores inválidos
+df = df[df['total_items'] > 0]
+df = df[df['total_cost'] > 0]
+
+# Remover duplicados
+df = df.drop_duplicates()
+
+# Salvar Parquet
+df.to_parquet("data/silver/retail_silver.parquet", index=False)
+
+print("Silver pronto")
