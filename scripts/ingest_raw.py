ac1cf1a829c8ce89df5fcf6a7cd587245dc51238
flg29-data <ferna29@gmail.com>
2026-03-24 02:17:50 -0300

    Update ingest_raw.py

--- scripts/ingest_raw.py
+++ scripts/ingest_raw.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # Arquivo baixado do Kaggle
-df = pd.read_csv("retail_transactions.csv", nrows=1000000)
+df = pd.read_csv("Retail_Transactions_Dataset.csv", nrows=1000000)
 
 # Salvar sem alterações
 df.to_csv("data/raw/retail_raw.csv", index=False)
